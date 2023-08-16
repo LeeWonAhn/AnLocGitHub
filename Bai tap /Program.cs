d339998d0b67be432a0e34d251bdf91ab740c336
locna <anloc0611@gmail.com>
2023-08-16 21:37:54 +0700

    C# căn bản lý thuyết

---
+++ Bai tap /Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        float width;
+        float height; 
+        Console.Write("Enter width: ");
+        width = float.Parse(Console.ReadLine());
+        Console.Write("Enter height: ");
+        height = float.Parse(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine("Area is: " + area);
+    }
+}
--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace Helloworld{
     class Program
     {
+        const int g = 10; // const khong the thay doi
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
-            Console.WriteLine("Nguyen An Loc");
+            // // Console.WriteLine("Hello, world!");
+            // // Console.WriteLine("Nguyen An Loc");
+            // // int v1 = 1;
+            // // char a = 'a';
+            // // bool a1 = true;
+            // // var a2 = 2; //var la bien dong.
+            // int v2 = 13%10;
+            // Console.WriteLine(v2);
+            // int i = 0;
+            // i++;
+            // i++;
+            // Console.WriteLine(i);
+            // bool daHocQuaTatCaTinChi = true;
+            // bool daCoBangDaiHoc = true;
+            // Console.WriteLine(daHocQuaTatCaTinChi&&daCoBangDaiHoc);
+            // // &&: Thoa man 2 dieu kien
+            // // ||: Thoa man 1 trong 2 dieu kien
+            // // !: Nguoc voi dieu kien
+
+            // int a = 1;
+            // a += 2;
+            // a= a + 2;
+            // Thuc Hanh
+            DateTime localDate = Datetime.Now;
+            Console.WriteLine("Date time now is: " + localDate);
         }
     }
 }
