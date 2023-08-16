ff28e4ada7312622d19289d2654d253c78253272
locna <anloc0611@gmail.com>
2023-08-16 20:18:36 +0700

    Hello World

---
+++ Hello World/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Helloworld{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Nguyen An Loc");
+        }
+    }
+}
