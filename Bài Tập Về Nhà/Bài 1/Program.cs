ca33ae5ea66cbd64189401969ab874ec24ed4460
locna <anloc0611@gmail.com>
2023-08-17 19:24:10 +0700

    Bai Tap Ve Nha

---
+++ Bài Tập Về Nhà/Bài 1/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your full name: ");
+        string fullName = Console.ReadLine();
+        Console.WriteLine("Welcome " + fullName);
+    }
+}
---
+++ Bài Tập Về Nhà/Bài 2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConvertUsdToVndApp
+{
+    class Bai2
+{
+    static void Main(string[] args)
+    {
+        int VND = 23000;
+        int USD;
+        Console.WriteLine("Enter your amount in USD: ");
+        string amount = Console.ReadLine();
+        USD = int.Parse(amount);
+        int vndAmount = VND * USD;
+        Console.WriteLine("Your amount is: " + vndAmount + "VND");
+    }
+}
+}
