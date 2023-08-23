25e1476896e6d6fb0ada615c09af84fc777f143a
locna <anloc0611@gmail.com>
2023-08-23 21:25:02 +0700

    Bai Hoc Buoi 3

--- Bai tap /Program.cs
+++ Bai tap /Program.cs
@@ -67,5 +67,31 @@
         }else if(delta > 0) {
             Console.WriteLine($"Phuong trinh co 2 nghiem: {(-b - (float)(Math.Sqrt(delta)))/(2*a)}, {(-b - (float)(Math.Sqrt(delta)))/(2*a)} ");
         }
+
+        // Bai tap Array
+        //Bai tap 1
+        int[] a ={3, 5, 7, 9};
+            int total = 0;
+            for (int i = 0; i < a.Length; i++) {
+                total += a[i];
+            }
+            Console.WriteLine("Total: " + total);
+        
+        //Bai Tap 2
+
+            string[] students = {"Christian", "Michael", 
+                                "Camila", "Sienna", 
+                                "Tanya", "Connor", 
+                                "Zachariah", "Mallory", 
+                                "Zoe", "Emily"};
+            Console.WriteLine("Enter Your Name: ");
+            string name = Console.ReadLine();
+            for (int i = 0; i < students.Length; i++){
+                if (name.ToUpper() == students[i].ToUpper()) {
+                    Console.WriteLine($"Da tim thay hoc sinh. Hoc sinh la phan tu thu {i} ");
+                    return;
+                }
+            }
+            Console.WriteLine("Khong tim thay duoc hoc sinh");
     }
 }
--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -70,19 +70,77 @@
             //     break;
             // }
             // Console.WriteLine(month_string);
-            Console.WriteLine("Nhap 1 so");
-            double a = Convert.ToDouble(Console.ReadLine());
-            while (a!=0)
-            {
-                Console.WriteLine($"a: {a}");
-                a--;
-            }
-            // do
+            // Console.WriteLine("Nhap 1 so");
+            // double a = Convert.ToDouble(Console.ReadLine());
+            // while (a!=0)
             // {
-            //     Console.WriteLine("Moi Nhap Lai");
-            //     a = Convert.ToDouble(Console.ReadLine());
-            // } while (a==0);
-            Console.WriteLine($"a: {a}");
-        }
+            //     Console.WriteLine($"a: {a}");
+            //     a--;
+            // }
+            // // do
+            // // {
+            // //     Console.WriteLine("Moi Nhap Lai");
+            // //     a = Convert.ToDouble(Console.ReadLine());
+            // // } while (a==0);
+            // Console.WriteLine($"a: {a}");
+            // for (int i = 0; i <10; i++) {
+            //  Console.WriteLine(i);
+            // }
+            // int width = 10;
+            // int width_temp = width;
+            // // int height = 5;
+            // // for (int i = 0; i < height; i++)
+            // // {
+            // //     for(int j = 0; j < width; j++){
+            // //         Console.Write("o");
+            // //     }
+            // //     Console.WriteLine("");
+            // // }
+            // for (int i = 0; i < width; i++)
+            // {
+            //     for (int j = 0; j < width_temp; j++)
+            //     {
+            //     Console.Write("o");
+            //     }
+            //     width_temp--;
+            //     Console.WriteLine("");
+            // }
+            // int a = 8;
+            // Console.WriteLine("Enter your number: ");
+            // a = Convert.ToInt32(Console.ReadLine());
+            // bool isPrime = true;
+            // for (int i = 2; i < a; i++)
+            // {
+            //     if (a % i == 0)
+            //     {
+            //         isPrime = false;
+            //     }
+            // }
+            // Console.WriteLine(isPrime);
+            // string[] player = new string[]{};
+            // int[] mang_so = new int[3];
+            // string[] mang_chu = new string[3];
+            // Console.WriteLine(mang_so[0]);
+            // Console.WriteLine(mang_chu[0]);
+            // Console.WriteLine(player[1]);
+            // player[0] = "An Loc";
+            // int[] diem_cac_man = new int[] {22, 23, 63};
+            // int total = 0;
+            // for (int i = 0; i < diem_cac_man.Length; i++)
+            // {
+            //     total += diem_cac_man[i];
+            // }
+            // Console.WriteLine("Total: " + total);
+            double[,] diem_cac_lop = {{1,3,2}, {3.3, 4.2, 4}};
+            // double diem = diem_cac_lop[0, 2];
+            // double diem4 = diem_cac_lop[1, 2];
+            for (int i = 0; i< diem_cac_lop.GetLength(0); i++){
+                for (int j = 0; j < diem_cac_lop.GetLength(1); j++)
+            {
+                    Console.Write(diem_cac_lop[i, j] + " ");
+            }
+            Console.WriteLine();
+            }
     }
+}
 }
