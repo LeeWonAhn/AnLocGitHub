7f82047722d37edce9e6aa6a1b0784b60b2a28b6
locna <anloc0611@gmail.com>
2023-08-23 16:05:18 +0700

    Bài học buổi 3

--- Bai tap /Program.cs
+++ Bai tap /Program.cs
@@ -2,13 +2,70 @@
 {
     static void Main(string[] args)
     {
-        float width;
-        float height; 
-        Console.Write("Enter width: ");
-        width = float.Parse(Console.ReadLine());
-        Console.Write("Enter height: ");
-        height = float.Parse(Console.ReadLine());
-        float area = width * height;
-        Console.WriteLine("Area is: " + area);
+        // float width;
+        // float height; 
+        // Console.Write("Enter width: ");
+        // width = float.Parse(Console.ReadLine());
+        // Console.Write("Enter height: ");
+        // height = float.Parse(Console.ReadLine());
+        // float area = width * height;
+        // Console.WriteLine("Area is: " + area);
+        // double a;
+        // double b;
+        // double c;
+        // // a=bx+c
+        // Console.WriteLine("Linear Equation Resolver a=bx+c");
+        // Console.WriteLine("a: ");
+        // a = Convert.ToDouble(Console.ReadLine());
+        // Console.WriteLine("b: ");
+        // b = Convert.ToDouble(Console.ReadLine());
+        // Console.WriteLine("c: ");
+        // c = Convert.ToDouble(Console.ReadLine());
+        // if (b==0){
+        //     if (a==c){
+        //         Console.WriteLine("Phuong Trinh Vo So Nghiem");
+        //     }else
+        //     {
+        //         Console.WriteLine("Phuong trinh vo nghiem");
+        //     }
+        // }else {
+        //     double x = (a-c)/b;
+        //     Console.WriteLine($"Result {a} = {b}.x + {c} la {x}");
+        // }
+        // double weight;
+        // double height;
+        // Console.WriteLine("Enter your Weight");
+        // weight = float.Parse(Console.ReadLine());
+        // Console.WriteLine("Enter your Height");
+        // height = float.Parse(Console.ReadLine());
+        // double bmi = weight / Math.Pow(height,2);
+        // bmi = Math.Round(bmi, 1);
+        // Console.Write("BMI: "+ bmi);
+        //     if (bmi < 18)
+        //         {Console.WriteLine(" Underweight");}
+        //     else if (bmi < 25.0)
+        //         {Console.WriteLine(" Normal");}
+        //     else if (bmi < 30.0)
+        //         {Console.WriteLine(" Overweight");}
+        //     else
+        //         {Console.WriteLine(" Obese");}
+        Console.WriteLine("Giai phuong trinh bac 2: ax2 + bx + c = 0");
+        Console.WriteLine("Enter number a: ");
+        float a = float.Parse(Console.ReadLine());
+        Console.WriteLine("Enter number b: ");
+        float b = float.Parse(Console.ReadLine());
+        Console.WriteLine("Enter number c: ");
+        float c = float.Parse(Console.ReadLine());
+
+        double delta = Math.Pow(b,2) - 4*a*c;
+
+        if (delta < 0)
+        {
+            Console.WriteLine("Phuong trinh vo nghiem");
+        }else if(delta == 0) {
+            Console.WriteLine($"Phuong trinh co nghiem kep: {-b/(2*a)}");
+        }else if(delta > 0) {
+            Console.WriteLine($"Phuong trinh co 2 nghiem: {(-b - (float)(Math.Sqrt(delta)))/(2*a)}, {(-b - (float)(Math.Sqrt(delta)))/(2*a)} ");
+        }
     }
 }
--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -27,8 +27,62 @@
             // a += 2;
             // a= a + 2;
             // Thuc Hanh
-            DateTime localDate = Datetime.Now;
-            Console.WriteLine("Date time now is: " + localDate);
+            // DateTime localDate = Datetime.Now;
+            // Console.WriteLine("Date time now is: " + localDate);
+            // int an_loc = 13;
+            // if (an_loc >= 18)
+            // {
+            //     Console.WriteLine("Du Dieu Kien Uong Bia");
+            // } else
+            // {
+            //      Console.WriteLine("Khong Du Dieu Kien Uong Bia");
+            // }
+            // int age = 7;
+            // if (age>=6 && age <11)
+            // {
+            //     Console.WriteLine("Dang hoc tieu hoc");
+            // }else if(age > 11 && age <=15)
+            // {
+            //     Console.WriteLine("Dang hoc cap 2");
+            // }else if(age >= 15 && age<18)
+            // {
+            //     Console.WriteLine("Dang hoc cap 3");
+            // }else
+            // {
+                
+            // }
+            // Console.WriteLine("Ket thuc chuong trinh");
+            // int month = 1;
+            // string month_string = "";
+            // switch (month)
+            // {
+            // case 1: 
+            //     month_string = "January";
+            //     break;
+            // case 2:
+            //     month_string = "February";
+            //     break;
+            // case 3:
+            //     month_string = "March";
+            //     break;
+            // default:
+            //     month_string = "December";
+            //     break;
+            // }
+            // Console.WriteLine(month_string);
+            Console.WriteLine("Nhap 1 so");
+            double a = Convert.ToDouble(Console.ReadLine());
+            while (a!=0)
+            {
+                Console.WriteLine($"a: {a}");
+                a--;
+            }
+            // do
+            // {
+            //     Console.WriteLine("Moi Nhap Lai");
+            //     a = Convert.ToDouble(Console.ReadLine());
+            // } while (a==0);
+            Console.WriteLine($"a: {a}");
         }
     }
 }
