07a025d6bb59a3d44b8221199f053a203c05f986
locna <anloc0611@gmail.com>
2023-08-30 09:07:59 +0700

    Bài tập về nhà buổi 5

---
+++ Bài Tập Về Nhà/Bài tập buổi 5.1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Name
+{
+    class Program
+    {
+       static int FindMaxValue(int[,] array)
+    {
+        int maxValue = array[0, 0];
+
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (array[i, j] > maxValue)
+                {
+                    maxValue = array[i, j];
+                }
+            }
+        }
+
+        return maxValue;
+    }
+
+    static void Main(string[] args)
+    {
+        int[,] matrix = {
+            { 5, 6, 7 },
+            { 8, 9, 10 },
+            { 11, 12, 13 }
+        }; // Mảng hai chiều
+
+        int maxElement = FindMaxValue(matrix);
+
+        Console.WriteLine("Max value in the 2D array: " + maxElement);
+    }
+    }
+}
---
+++ Bài Tập Về Nhà/Bài tập buổi 5.2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static int CalculateMainDiagonalSum(int[,] array)
+        {
+            int sum = 0;
+
+            int size = array.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += array[i, i];
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] matrix = {
+            { 5, 6, 7 },
+            { 8, 9, 10 },
+            { 11, 12, 13 }
+        };
+
+            int diagonalSum = CalculateMainDiagonalSum(matrix);
+
+            Console.WriteLine("Sum of main diagonal elements: " + diagonalSum);
+        }
+    }
+}
